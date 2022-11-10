c51ed453f14908f9c03895fa409e4624ece04353
majidhameed <majid.hameed@gmail.com>
2022-11-10 22:03:55 +0100

    Added the documentation using open api.

--- src/main/java/ag/egroup/issuetracker/advice/ApplicationAdvice.java
+++ src/main/java/ag/egroup/issuetracker/advice/ApplicationAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
